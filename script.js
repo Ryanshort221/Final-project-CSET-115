00a061e6745b3db4011ebe527b0512279f0fd727
Ryan Short <ryanshort55@gmail.com>
2022-11-11 09:01:15 -0500

    trying to get username check working

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let activeUsers = [];
+let userNames = [];
 const submitBtn = document.querySelector('#submitBtn');
 submitBtn.addEventListener('click', () => {
     const user = document.querySelector('#username').value;
     const userPassword = document.querySelector('#password').value;
-    activeUsers.push({user, userPassword});
-    console.log(activeUsers);
-});
-
