4d55aa3620e911be2b736435fcd34beaad3700be
Ryan Short <ryanshort55@gmail.com>
2022-11-11 09:03:39 -0500

    working on js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let activeUsers = [];
-let userNames = [];
 const submitBtn = document.querySelector('#submitBtn');
 submitBtn.addEventListener('click', () => {
     const user = document.querySelector('#username').value;
     const userPassword = document.querySelector('#password').value;
+    const userObj = {user, userPassword};
+    activeUsers.push(userObj);
+    console.log(activeUsers);
+});
