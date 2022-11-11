7534b778b914c647da4b8b03971e6f7536cd2b34
Doogle <douglashwartz@gmail.com>
2022-11-11 09:01:07 -0800

    added a way to see is usernames are already in use

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let activeUsers = [];
+let Users = []
 const submitBtn = document.querySelector('#submitBtn');
 submitBtn.addEventListener('click', () => {
     const user = document.querySelector('#username').value;
     const userPassword = document.querySelector('#password').value;
-    activeUsers.push({user, userPassword});
+    if(Users.indexOf(user)===-1){
+        activeUsers.push({user, userPassword});
+        Users.push(user)
+    }else{
+        alert("Username already in use")
+    }
+        
     console.log(activeUsers);
 });
 
